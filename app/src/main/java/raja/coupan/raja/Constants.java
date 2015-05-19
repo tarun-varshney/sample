5d0c380045d6db6e13685509964e006c81fff196
Tarun <tarun.varshney@redbus.in>
2015-05-19 23:23:44 +0530

    First Commit

---
+++ app/src/main/java/raja/coupan/raja/Constants.java
@@ -0,0 +1,26 @@
+package raja.coupan.raja;
+
+/**
+ * Created by Tarun Varshney on 5/18/2015.
+ */
+public class Constants {
+
+    public static String BASE_URL = "http://api.couponraja.in/JSonService.asmx/";
+
+    //All data for fist page
+    public static String HOME_PAGE_URL = "getCatSubcatTopnewVoucherandtopstoreDetails?";
+
+    //Home See More Coupon
+    public static String MORE_COUPON_HOME_URL = "GetHomeMoreCouponData_By_prmhome?";
+
+    // Top Store Individual Item Click
+    public static String COUPON_DATA_MERCHANT_URL = "GetmerchantCouponData_By_prmhome?";
+
+    //For Category Page
+    public static String COUPON_DATA_CATEGORY_URL = "GetcategoryCouponData_By_prmhome?";
+
+    //Category  Page Filter click
+    public static String COUPON_DATA_SUBCAT_URL =  "GetsubcatTopStores_bycategory?";
+
+
+}
